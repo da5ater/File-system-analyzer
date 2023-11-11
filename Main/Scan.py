e4c413d5cb72b384d61800d2b0f7d88cad771ff4
Mohamed <107060533+da5ater@users.noreply.github.com>
2023-11-11 20:29:13 +0200

    Update Scan.py
    Used  os.listdir() to get a list of all the content on the directory

--- Main/Scan.py
+++ Main/Scan.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import humanize
+from pathlib import Path as p
 
 
 
@@ -8,16 +9,18 @@ import humanize
 # TAKES A PATH AND RETURN INFORMATION
 # [ input is path and it is -> string ]   [ out  ??] 
 def scan(path):
+    # path = p(path)
     # CHECK if path is empty or not a string
     if not path or not isinstance(path,str):
         raise ValueError("please Provide a valid Path")
-    
+
     try :
     # validate pass existance
         if not os.path.exists(path):
             raise FileNotFoundError("path dose not exist")
     except PermissionError as e:
         raise PermissionError("Permission eror accessing '{path}' : '{e}' ")
+    
     try :
     # CHECK IF PATH IS NOT A DIRECTORY
         if not os.path.isdir(path):
@@ -25,8 +28,23 @@ def scan(path):
     except PermissionError as e:
         raise PermissionError("PROVIDED PATH  '{path}' IS NOT A DIRECTORY : '{e}' ")
     
-    
+    try :
+    # getting a list of all the directories
+        content = os.listdir(path)
+    except FileNotFoundError as e :
+        print("eror : '{e}")
+    except PermissionError as e :
+        print("erorr : '{e}")
+
+
+    print(content)
+
+scan("G:\Mohamed\هجرة")
+
+     
+
+
+
     
         
     
-scan("d")
