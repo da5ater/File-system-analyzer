232f83a360a2aad0140bc6d323635e27fdc2a9e1
Mohamed <107060533+da5ater@users.noreply.github.com>
2023-11-12 15:31:08 +0200

    added a dictionary that hold file nam as keys and file information as values
    updated the extract function to build a dictionary and assign each file as a key and all of the file information as values respectively

--- Main/Scan.py
+++ Main/Scan.py
@@ -1,28 +1,72 @@
 import os
-import datetime
-import humanize
-from pathlib import Path as p
+from datetime import datetime 
+
+
 
 
 # extract the info of the files in the path given recursivly
 # (in  -> content(string), path(string))   (out  -> file_info(list))
 def extract_info(content,path):
-    for entry in content:
-        entry_path = os.path.join(path ,entry)
-        if os.path.isfile(entry_path):
-            print(entry_path)
-            file_info = os.stat(entry_path)            
-            return file_info.st_size    
-        else :
-            file_info = scan(entry_path)
 
+    # a dictionary contain a key wich is the file (string), and a value wich is all the informatio in a readable way (list) 
+    entries_info = {}
+    try:
+        for entry in content:
 
+            entry_path = os.path.join(path ,entry)
+
+            if os.path.isfile(entry_path):
+                # a list containes all the information about the file
+                array_of_information=[]    
+
+                
+                file_info = os.stat(entry_path) 
+
+                # size information
+                file_size = file_info.st_size
+                # size in a human_readable way
+                size =  str(file_size / 1024 )+ ' KB'
+                # adding it up in the list
+                array_of_information.append(size)
+
+                # time information
+                file_creation_time = file_info.st_ctime
+                file_modefication_time = file_info.st_mtime
+                # time in a human-readable way
+                creation_time = datetime.fromtimestamp(file_creation_time).strftime('%Y-%m-%d %H:%M:%S')
+                # adding it up in the list
+                array_of_information.append(creation_time)
+                modefication_time = datetime.fromtimestamp(file_modefication_time).strftime('%Y-%m-%d %H:%M:%S')
+                # adding it up in the list
+                array_of_information.append(modefication_time)
+
+                # admenestrative information
+                file_owner = file_info.st_uid
+                # adding it up in the list
+                array_of_information.append(file_owner)
+                file_permission = file_info.st_mode
+                # adding it up in the list
+                array_of_information.append(file_permission)
+
+                # assign the file with the array of information
+                entries_info[entry] = array_of_information
+
+                del(array_of_information)
+                    
+            else :
+                file_info = scan(entry_path)
+                
+    except PermissionError as e:
+        print("permission erorr : '{e}")                
+
+    for x in entries_info:
+        print(x)
 
 # a function to scan a given path recursively 
 # TAKES A PATH AND RETURN INFORMATION
 # [ input is path and it is -> string ]   [ out  ??] 
 def scan(path):
-    # path = p(path)
+    
     # CHECK if path is empty or not a string
     if not path or not isinstance(path,str):
         raise ValueError("please Provide a valid Path")
@@ -41,6 +85,10 @@ def scan(path):
     except PermissionError as e:
         raise PermissionError("PROVIDED PATH  '{path}' IS NOT A DIRECTORY : '{e}' ")
     
+ 
+    content = os.listdir(path)
+
+
     try :
     # getting a list of all the directories
         content = os.listdir(path)
@@ -50,10 +98,10 @@ def scan(path):
         print("erorr : '{e}")
     
 
-    print(extract_info(content,path))
+    extract_info(content,path)
 
 
-scan(r'G:\softwares')
+scan(r"G:\\")
    
         
 
